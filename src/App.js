aeaf38844c5dd7251d6f4655723893d9ec8fd56b
hyeonji-d <hyeonji14kim@gmail.com>
2022-11-24 20:31:14 +0900

    1124

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
+import CPU from './components/pages/CPU';
+import Memory from './components/pages/Memory';
+import HDD from './components/pages/HDD';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cpu" element={<CPU />} />
+          <Route path="/memory" element={<Memory />} />
+          <Route path="/hdd" element={<HDD />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
